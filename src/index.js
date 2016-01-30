e33bad811c95bca798845fc932cf4e9888cea4ff
h3x3d <h3x3d@ya.ru>
2016-01-31 02:12:47 +0300

    Eslint fixes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ export class GenericNotImplemented extends Error {
    * @param  {String} method   Method name
    * @param  {[type]} dispatch Dispatch value
    */
-  constructor(method, dispatch = null){
-    super(`Generic method '${method}' is not implemented` + (dispatch ? ` for dispatch value '${dispatch}'` : ''));
+  constructor(method, dispatch = null) {
+    super(`Generic method '${method}'(${dispatch}) is not implemented`);
     Object.defineProperty(this, 'method', {
       value: method,
       writable: false,
-      configurable: false
+      configurable: false,
     });
     Object.defineProperty(this, 'dispatch', {
       value: dispatch,
       writable: false,
-      configurable: false
+      configurable: false,
     });
   }
 }
@@ -27,27 +27,27 @@ export class GenericNotImplemented extends Error {
  * @param  {String} name Name of new namespace
  * @return {Object}      Namespace to pass into defgeneric & defmethod
  */
-export function namespace(name = undefined){
-  return name ? {_name: name} : {};
+export function namespace(name = undefined) {
+  return name ? { _name: name } : {};
 }
 
 /**
  * Registers new generic method inside provided namespace
- * @param  {Object} namespace  Namespace to use
+ * @param  {Object} ns  Namespace to use
  * @param  {String} name       Name of generic method
  * @param  {Function} dispatcher Dispatcher function
  * @return {Function}          Created generic method
  */
-export function defgeneric(namespace, name, dispatcher){
-  namespace[name] = new Map();
-  let fn = function(...args) {
-    let val = dispatcher.apply(null, args);
-    if(!namespace[name].has(val)){
+export function defgeneric(ns, name, dispatcher) {
+  ns[name] = new Map();
+  const fn = (...args) => {
+    const val = dispatcher.apply(null, args);
+    if (!ns[name].has(val)) {
       throw new GenericNotImplemented(name, val);
     }
-    return namespace[name].get(val).apply(this, args);
-  }
-  Object.defineProperty(fn, 'name', {value: name});
+    return ns[name].get(val).apply(this, args);
+  };
+  Object.defineProperty(fn, 'name', { value: name });
   return fn;
 }
 
@@ -59,10 +59,10 @@ export function defgeneric(namespace, name, dispatcher){
  * @param  {Function} imp           Method implementation
  * @return {Function}               Implementation
  */
-export function defmethod(namespace, name, dispatchValue, imp){
-  if(!namespace[name]){
+export function defmethod(ns, name, dispatchValue, imp) {
+  if (!ns[name]) {
     throw new GenericNotImplemented(name);
   }
-  namespace[name].set(dispatchValue, imp);
+  ns[name].set(dispatchValue, imp);
   return imp;
 }
